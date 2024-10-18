0dadfc34280c1e9502b2d08ed60265d92ab4b96a
felipeggoncales <felipeggoncales@gmail.com>
2024-10-18 15:30:20 -0300

    responsivo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function abrirFecharMenu() {
     const linha1 = document.getElementById('linha1');
     const linha2 = document.getElementById('linha2');
+    const linha3 = document.getElementById('linha3');
     const overlay = document.querySelector('.overlay');
     const itensMenu = document.querySelector('.div-items-menu');
 
@@ -9,24 +10,34 @@ function abrirFecharMenu() {
     if (linhaDisplay === 'static') {
         linha1.style.position = 'absolute';
         linha1.style.transform = 'rotate(-45deg)';
-        linha2.style.position = 'absolute';
-        linha2.style.transform = 'rotate(45deg)';
-        if (!overlay.classList.contains('mostrar')) {
-            overlay.classList.add('mostrar');
-        }
-        overlay.classList.replace('esconder','mostrar');
-        if (!itensMenu.classList.contains('surgir-menu')) {
-            itensMenu.classList.add('surgir-menu');
-        }
-        itensMenu.style.display = 'flex';
-        itensMenu.classList.replace('sumir-menu','surgir-menu');
+        linha2.style.display = 'none';
+        linha3.style.position = 'absolute';
+        linha3.style.transform = 'rotate(45deg)';
+        overlay.style.display = 'flex';
+        setTimeout(()=> {
+            if (!overlay.classList.contains('mostrar')) {
+                overlay.classList.add('mostrar');
+            };
+            overlay.classList.replace('esconder','mostrar');
+            if (!itensMenu.classList.contains('surgir-menu')) {
+                itensMenu.classList.add('surgir-menu');
+            };
+            itensMenu.style.display = 'flex';
+            itensMenu.classList.replace('sumir-menu','surgir-menu');
+        },5);
         
     } else {
         linha1.style.position = 'static';
         linha1.style.transform = 'rotate(0deg)';
-        linha2.style.position = 'static';
-        linha2.style.transform = 'rotate(0deg)';
+        linha2.style.display = 'block';
+        linha3.style.position = 'static';
+        linha3.style.transform = 'rotate(0deg)';
         itensMenu.classList.replace('surgir-menu','sumir-menu');
         overlay.classList.replace('mostrar','esconder');
-    }
-}
+        setTimeout(()=> {
+            if (overlay.classList.contains('esconder')) {
+                overlay.style.display = 'none';
+            }
+        },500);
+    };
+};
