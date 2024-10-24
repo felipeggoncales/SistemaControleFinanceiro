c27f51de09aa052770cdad51081e6b6ba7ca0965
Felipe Gonçales <felipeggoncales@gmail.com>
2024-10-24 18:58:17 -0300

    alterações

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ const linha2 = document.getElementById('linha2');
 const linha3 = document.getElementById('linha3');
 const overlay = document.getElementById('overlay');
 const itensMenu = document.querySelector('.div-items-menu');
+var limiteMensal = 1000;
+let gastos = 999.5 // Variável a ser trocada quando a função de cadastar despesas for implemantada
+var porcentagemLimiteConcluido = 75;
+
+const limiteTexto = document.getElementById('porcentagem');
+const waves = document.getElementsByClassName('wave');
+const loadingBar = document.querySelector('.div-loading');
+const porcentagemTexto = document.getElementById('porcentagem');
+const limiteMensagem = document.getElementById('limiteMsg');
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!limiteMensal) {
+        limiteTexto.textContent = '?';
+        limiteMensagem.textContent = 'Defina seu limite';
+        alterarGraficoLimite();
+    } else {
+        const parentHeight = window.getComputedStyle(loadingBar.parentNode).height;
+        porcentagemLimiteConcluido = ((gastos / parseInt(limiteMensal))*100).toFixed(1); // Ainda não existe, precisa fazer o banco SQL
+        porcentagemTexto.textContent = `${porcentagemLimiteConcluido}%`
+        alterarGraficoLimite();
+    }
+})
+
+function alterarGraficoLimite() {
+    Array.from(waves).forEach(wave => {
+        wave.style.bottom = `calc(${porcentagemLimiteConcluido}% - 30px)`;
+    });
+    loadingBar.style.height = `calc(${porcentagemLimiteConcluido}% - 30px)`;
+}
 
 function abrirFecharMenu() {
     const linhaDisplay = getComputedStyle(linha1).position;
@@ -58,17 +87,35 @@ function abrirFecharDivLimite() {
     };
 }
 
-var limiteMensal = null;
 const inputLimite = document.getElementById('limiteInput');
 const salvarLimiteButton =  document.getElementById('salvarLimite');
+
 salvarLimiteButton.addEventListener('click', function() {
-    limiteMensal = parseInt(inputLimite);
-    exibirMensagem();
+    if (inputLimite.value) {
+        limiteMensal = parseInt(inputLimite.value);
+        exibirMensagem(limiteMensal);
+    }
 })
 
 const mensagemSalvo = document.getElementById('mensagemLimiteSalvo');
 const limiteValor = document.getElementById('limiteValor');
-function exibirMensagem() {
-    limiteValor.textContent = String(limiteMensal);
+let animacaoAtiva = false;
+
+function exibirMensagem(mensagem) {
+    if (animacaoAtiva) return;
+    
+    animacaoAtiva = true;
+
+    limiteValor.textContent = mensagem;
+    mensagemSalvo.classList.add('mensagemSurgir');
     mensagemSalvo.style.display = 'flex';
+
+    setTimeout(() => {
+        mensagemSalvo.classList.replace('mensagemSurgir', 'mensagemSumir');
+        setTimeout(() => {
+            mensagemSalvo.style.display = 'none';
+            mensagemSalvo.classList.remove('mensagemSumir');
+            animacaoAtiva = false;
+        }, 500);
+    }, 5000);
 }
